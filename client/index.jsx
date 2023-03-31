cb2dc76ffc3632c7025ac46e69e01c3a35f76f76
ian <ianr.hnizdo@gmail.com>
2023-03-31 16:32:17 -0400

    Finally got React linked with Webpack.

--- client/index.jsx
+++ client/index.jsx
@@ -3,12 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <p>This is being rendered from React!</p>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
 
-// reportWebVitals();
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./client/index.jsx",
+  entry: path.resolve(__dirname, "./client/index.jsx"),
   output: {
-    path: __dirname + "/dist",
+    path: path.resolve(__dirname, "./build/"),
     filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, "./public"),
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)?$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
@@ -18,9 +27,13 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js', '.jsx'],
-  }
+    extensions: ["*", ".js", ".jsx"],
+  },
 };
